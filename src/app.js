d449c182fd0928b9d764e695790a5fb869a228c0
Manu214609 <150542974+Manu214609@users.noreply.github.com>
2024-02-26 21:15:51 +0000

    termine

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardsValues = ["2", "3", "4", "8", "6", "J"];
+  let cardsTypes = ["♦", "♥", "♠", "♣"];
+  let cardValueElement = document.getElementById("cardValue");
+  let cardTypeTopElement = document.getElementById("cardTypeTop");
+  let cardTypeBottomElement = document.getElementById("cardTypeBottom");
+
+  function getRandomCard() {
+    let randomValueIndex = Math.floor(Math.random() * cardsValues.length);
+    let randomValue = cardsValues[randomValueIndex];
+    let randomTypesIndex = Math.floor(Math.random() * cardsTypes.length);
+    let randomTypes = cardsTypes[randomTypesIndex];
+    cardTypeBottomElement.textContent = randomTypes;
+    cardTypeTopElement.textContent = randomTypes;
+    cardValueElement.textContent = randomValue;
+
+    if (randomTypes == "♥") {
+      cardTypeBottomElement.style.color = "red";
+      cardTypeTopElement.style.color = "red";
+    }
+  }
+  getRandomCard();
 };
